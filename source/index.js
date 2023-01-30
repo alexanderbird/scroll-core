4d57c2ed2aba4e9d88bf61829c382260cbf51693
Alexander Bird <alex@alexanderbird.software>
2023-01-30 06:20:24 -0800

    Export short identifier from index.js

--- source/index.js
+++ source/index.js
@@ -1,4 +1,5 @@
 const { identifier, reference } = require('./core/identifier');
 const { jump } = require('./core/jump');
+const shortIdentifier = require('./core/shortIdentifier');
 
-module.exports = { reference, identifier, jump };
+module.exports = { reference, identifier, jump, shortIdentifier };
