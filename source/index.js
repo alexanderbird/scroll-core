5c36df2c23edecedc6357171f74e76de446ea2d1
Alexander Bird <alex@alexanderbird.software>
2024-03-03 18:31:42 -0800

    Export everything required for search

--- source/index.js
+++ source/index.js
@@ -1,5 +1,7 @@
 const { identifier, reference } = require('./core/identifier');
 const { jump } = require('./core/jump');
+const { tokenize } = require('./search/tokenize');
 const shortIdentifier = require('./core/shortIdentifier');
+const searchIndex = require('../search-index/webp.json');
 
-module.exports = { reference, identifier, jump, shortIdentifier };
+module.exports = { reference, identifier, jump, shortIdentifier, tokenizeForSearch: tokenize, searchIndex };
